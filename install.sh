047420148c05ca9e82c8bc61628b5525464fddc4
CyberFutureCloud <kokosk399@gmail.com>
2024-12-28 12:02:54 +0100

    install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ download_file() {
 
 # Instalacja wymaganych pakietów
 echo "Instalowanie wymaganych pakietów..."
-required_packages=("curl" "wget")
+required_packages=("curl" "wget" "jq")
 for pkg in "${required_packages[@]}"; do
     if ! command -v $pkg &> /dev/null; then
         echo "$pkg nie jest zainstalowany. Instalowanie..."
@@ -27,22 +27,35 @@ for pkg in "${required_packages[@]}"; do
     fi
 done
 
-# Weryfikacja klucza licencyjnego
+# Weryfikacja klucza licencyjnego i emaila
 read -p "Wprowadź klucz licencyjny: " license_key
+read -p "Wprowadź swój email: " email
+
+SUPABASE_URL="https://qfsnfurfqvhychkmxlvl.supabase.co"
+SUPABASE_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFmc25mdXJmcXZoeWNoa214bHZsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUzMzY0OTIsImV4cCI6MjA1MDkxMjQ5Mn0.l14dBfHv1yW01VuRWViqOFlojOcLdVpfWCI92AuAbxI"
 
 verify_license_online() {
     key=$1
-    response=$(curl -s -X POST -H "Content-Type: application/json" -d '{"license_key": "'$key'"}' https://your-license-server.com/api/verify)
-    if [[ "$response" == *"\"valid\":true"* ]]; then
+    email=$2
+
+    response=$(curl -s -X POST \
+        -H "apikey: $SUPABASE_KEY" \
+        -H "Authorization: Bearer $SUPABASE_KEY" \
+        -H "Content-Type: application/json" \
+        -d '{"license_key": "'"$key"'", "email": "'"$email"'"}' \
+        "$SUPABASE_URL/rest/v1/rpc/verify_license")
+
+    valid=$(echo "$response" | jq -r '.valid')
+    if [ "$valid" == "true" ]; then
         echo "Licencja zweryfikowana pomyślnie."
         return 0
     else
-        echo "Nieprawidłowy klucz licencyjny."
+        echo "Nieprawidłowy klucz licencyjny lub email."
         return 1
     fi
 }
 
-verify_license_online $license_key
+verify_license_online $license_key $email
 if [ $? -ne 0 ]; then
     exit 1
 fi
@@ -53,24 +66,17 @@ mkdir -p $config_dir
 
 # Zapisz konfigurację do pliku
 echo "Zapisuję dane konfiguracyjne..."
-echo "{\"license_key\": \"$license_key\", \"language\": \"English\"}" > $config_dir/config.json
+echo "{\"license_key\": \"$license_key\", \"email\": \"$email\", \"language\": \"English\"}" > $config_dir/config.json
 
 # Pobranie bota
 bot_url="https://www.mediafire.com/file/u5cm3pmjf3e2ygp/CyberGuard.py/file"
 bot_destination="$config_dir/cyberguardian_bot.py"
 download_file $bot_url $bot_destination
 
-# Tworzenie bazy danych
-database_file="$config_dir/cyberguardian.db"
-if [ ! -f "$database_file" ]; then
-    echo "Tworzenie bazy danych..."
-    sqlite3 $database_file "CREATE TABLE licenses (key TEXT PRIMARY KEY, valid_until DATE);"
-    sqlite3 $database_file "INSERT INTO licenses (key, valid_until) VALUES ('$license_key', '2025-12-31');"
-fi
-
 # Uruchomienie bota
 echo "Uruchamianie bota..."
 python3 $bot_destination &
 
 echo "Instalacja zakończona sukcesem! Bot został uruchomiony."
+
 
